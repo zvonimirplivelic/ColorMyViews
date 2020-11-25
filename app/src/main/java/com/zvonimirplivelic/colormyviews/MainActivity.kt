5567d43fdc950058cbfd8a0594df749d0717c5dd
Zvonimir <zvonimirplivelic91@gmail.com>
2020-11-25 10:05:25 +0100

    removed action bar, added all views, layout done

--- app/src/main/java/com/zvonimirplivelic/colormyviews/MainActivity.kt
+++ app/src/main/java/com/zvonimirplivelic/colormyviews/MainActivity.kt
@@ -14,11 +14,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-        val boxOneText = findViewById<TextView>(R.id.box_one_text)
-        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
-        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
-        val boxFourText = findViewById<TextView>(R.id.box_four_text)
-        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        val boxOneText = findViewById<TextView>(R.id.box_one)
+        val boxTwoText = findViewById<TextView>(R.id.box_two)
+        val boxThreeText = findViewById<TextView>(R.id.box_three)
+        val boxFourText = findViewById<TextView>(R.id.box_four)
+        val boxFiveText = findViewById<TextView>(R.id.box_five)
         val constraintLayout = findViewById<View>(R.id.constraint_layout)
 
         val clickableViews: List<View> =
@@ -39,11 +39,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun makeColored(view: View) {
         when (view.id) {
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_four_text -> view.setBackgroundColor(Color.GREEN)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_one -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_four -> view.setBackgroundColor(Color.GREEN)
+            R.id.box_five -> view.setBackgroundColor(Color.BLUE)
             else -> view.setBackgroundColor(Color.RED)
         }
     }
